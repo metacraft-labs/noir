5133bd2dc14c6993e28d11c0317710289f708963
Alexander Ivanov <alehander42@gmail.com>
2025-02-07 22:52:19 +0200

    build: bump runtime_tracing in noir.nix

--- noir.nix
+++ noir.nix
@@ -24,7 +24,7 @@ in
         "clap-markdown-0.1.3" = "sha256-2vG7x+7T7FrymDvbsR35l4pVzgixxq9paXYNeKenrkQ=";
         "plonky2-0.2.0" = "sha256-2oheUUDu4ggNZEX9sF3Ef3PNrdFIUg5POeOFIEXEkUY=";
         "plonky2_u32-0.1.0" = "sha256-COTm1Fi90+vCnc1MnqyKh8/DVzo/B9VO2o0RQvE9/nM=";
-        "runtime_tracing-0.5.16" = "sha256-EUZV8WZvLw9kCXw7LRdbnNoq6CCuBst2PoQyES86bdY=";
+        "runtime_tracing-0.7.0" = "sha256-pSm+e2ly9dgK6T6ZwoJSMuD7726IwZtms+OzQffvTyU=";
       };
     };
 
